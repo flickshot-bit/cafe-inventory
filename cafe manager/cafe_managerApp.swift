1c2e3775cbb747d5f81cfbdfb1ed6e6ce1250d34
Aditya Kumar <adityak58551@gmail.com>
2026-04-30 20:42:05 +0530

    Initial Commit

---
+++ cafe manager/cafe_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cafe_managerApp.swift
+//  cafe manager
+//
+//  Created by Aditya kumar on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct cafe_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
